485d94d8b62060c914a82b015b589a174d4462f8
Herman Rademeyer <98677997+maans-dev@users.noreply.github.com>
2022-08-02 14:24:36 +0000

    Thanks Luke Haas for the deisgn @lukehaas

--- src/loading-spinner.js
+++ src/loading-spinner.js
@@ -1,38 +1,44 @@
-import { useState} from 'react'
-import Game from "./game";
-import LoadingSpinner from "./loadingspinner"
+import "./loading-spinner.css";
 
-function QuizPage() {
-//could create seperate useState//grouped values.
+/**
+ * @param {object} props
+ * @param {number|string} [props.size="3rem"] Width and height of the spinner.
+ * @param {number|string} [props.borderWidth="0.5em"] Thickness of the spinner.
+ * @param {string} [props.spinnerColor="#6060ff"] Color of the "active" part of the spinner.
+ * @param {string} [props.backgroundColor="rgba(86, 86, 86, 0.1)"] Color of the "inactive" part of
+ * the spinner.
+ * @param {string} [props.screenReaderMessage="Loading..."] Message to be displayed to screen
+ * readers.
+ * @param {React.CSSProperties} [props.style={}] Style to be applied to the spinner.
+ */
+function LoadingSpinner(props) {
+  const {
+    size = "3rem",
+    borderWidth = "0.5em",
+    spinnerColor = "#6060ff",
+    backgroundColor = "rgba(86, 86, 86, 0.1)",
+    screenReaderMessage = "Loading...",
+    style = {},
+  } = props;
 
-  const [quizFetch, setQuizFetch] = useState({
-    isLoading: true,
-    errorMessage: '',
-    data: null,
-  });
-
-const { isLoading, errorMessage, data } = quizFetch;
-
-let contents;
-if (isLoading) contents = <LoadingSpinner />;
-else if (errorMessage !== '') contents = <h1>error</h1>;
-else contents = <Game />;
+  const loaderStyle = {
+    width: size,
+    height: size,
+    borderColor: backgroundColor,
+    borderWidth,
+    borderLeftColor: spinnerColor,
+    ...style,
+  };
 
   return (
-    <main>
-     {contents}
-    </main>
+    <div className="loading-spinner">
+      <div
+        className="loading-spinner__spinner"
+        style={loaderStyle}
+        aria-label={screenReaderMessage}
+      />
+    </div>
   );
 }
 
-export default QuizPage;
-
-//also
-// {quizFetch.isLoading ? <LoadingSpinner />: null}
-
-//condition ? exprIfTrue : exprIfFalse
-// function example() {
-//   return condition1 ? value1
-//         : condition2 ? value2
-//         : condition3 ? value3
-//         : value4;
+export default LoadingSpinner;
